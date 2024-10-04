f723ec8796b1f75318e5ee4ab14065c1ad2dfb94
burak.ozer <burak.ozer@ankaref.com>
2024-10-04 17:27:50 +0300

    added features.

--- lib/room_page.dart
+++ lib/room_page.dart
@@ -12,31 +12,28 @@ class RoomPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Video Call App - $action"),
-        actions: [
-          if (action == 'create')
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Center(
+      body: Stack(
+        children: [
+          // Remote user video (Tam ekran)
+          ObxValue<Rx<RTCVideoRenderer>>(
+                (data) => Container(
+              width: double.infinity,
+              height: double.infinity,
+              child: data.value.srcObject != null
+                  ? RTCVideoView(
+                data.value,
+                objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover, // Tam ekran için
+              )
+                  : const Center(
                 child: Text(
-                  'Room ID: ${controller.roomId.value}',
-                  style: const TextStyle(fontSize: 16),
+                  "Waiting for remote user...",
+                  style: TextStyle(color: Colors.white),
                 ),
               ),
             ),
-        ],
-      ),
-      body: Stack(
-        children: [
-          ObxValue<Rx<RTCVideoRenderer>>(
-                (data) => Center(
-              child: data.value.srcObject != null
-                  ? RTCVideoView(data.value)
-                  : const Text("Waiting for remote user..."),
-            ),
             controller.remoteRenderer.obs,
           ),
+          // Local user video (Küçük pencere köşede)
           ObxValue<Rx<RTCVideoRenderer>>(
                 (data) => Positioned(
               bottom: 20,
@@ -44,7 +41,17 @@ class RoomPage extends StatelessWidget {
               width: 150,
               height: 200,
               child: data.value.srcObject != null
-                  ? RTCVideoView(data.value, mirror: true)
+                  ? Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 2), // Küçük video için kenarlık
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: RTCVideoView(
+                  data.value,
+                  mirror: true,
+                  objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
+                ),
+              )
                   : Container(
                 color: Colors.black,
                 child: const Center(
@@ -56,7 +63,25 @@ class RoomPage extends StatelessWidget {
               ),
             ),
             controller.localRenderer.obs,
-          )
+          ),
+          // Room ID (Sağ üstte, radiuslu)
+          Positioned(
+            top: 20,
+            right: 20,
+            child: Obx(
+                  () => Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text(
+                  'Room ID: ${controller.roomId.value}',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
