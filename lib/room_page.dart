771ea53a84fbce036a8e268995335b52ee7d906f
burak.ozer <burak.ozer@ankaref.com>
2024-10-04 17:41:55 +0300

    added features.

--- lib/room_page.dart
+++ lib/room_page.dart
@@ -9,62 +9,193 @@ class RoomPage extends StatelessWidget {
 
   RoomPage({super.key, required this.action});
 
+  final Rx<Offset> localVideoPosition = Offset(20, 20).obs;
+  final RxBool isLocalVideoLarge = false.obs;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          // Remote user video (Tam ekran)
+          // Remote user video
           ObxValue<Rx<RTCVideoRenderer>>(
-                (data) => Container(
-              width: double.infinity,
-              height: double.infinity,
-              child: data.value.srcObject != null
-                  ? RTCVideoView(
-                data.value,
-                objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover, // Tam ekran için
-              )
-                  : const Center(
-                child: Text(
-                  "Waiting for remote user...",
-                  style: TextStyle(color: Colors.white),
+                (data) => isLocalVideoLarge.value
+                ? Positioned(
+              left: localVideoPosition.value.dx,
+              top: localVideoPosition.value.dy,
+              child: Draggable(
+                feedback: Container(
+                  width: 150,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white, width: 2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: data.value.srcObject != null
+                      ? RTCVideoView(
+                    data.value,
+                    objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
+                  )
+                      : Container(
+                    color: Colors.black,
+                    child: const Center(
+                      child: Text(
+                        "Remote video",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+                childWhenDragging: Container(),
+                onDragEnd: (details) {
+                  localVideoPosition.value = details.offset;
+                },
+                child: GestureDetector(
+                  onTap: () {
+                    isLocalVideoLarge.value = !isLocalVideoLarge.value;
+                  },
+                  child: Container(
+                    width: 150,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: data.value.srcObject != null
+                        ? RTCVideoView(
+                      data.value,
+                      objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
+                    )
+                        : Container(
+                      color: Colors.black,
+                      child: const Center(
+                        child: Text(
+                          "Remote video",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ),
-            ),
-            controller.remoteRenderer.obs,
-          ),
-          // Local user video (Küçük pencere köşede)
-          ObxValue<Rx<RTCVideoRenderer>>(
-                (data) => Positioned(
-              bottom: 20,
-              right: 20,
-              width: 150,
-              height: 200,
-              child: data.value.srcObject != null
-                  ? Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white, width: 2), // Küçük video için kenarlık
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: RTCVideoView(
+            )
+                : Positioned.fill(
+              child: GestureDetector(
+                onTap: () {
+                  isLocalVideoLarge.value = !isLocalVideoLarge.value;
+                },
+                child: data.value.srcObject != null
+                    ? RTCVideoView(
                   data.value,
-                  mirror: true,
                   objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
-                ),
-              )
-                  : Container(
-                color: Colors.black,
-                child: const Center(
+                )
+                    : const Center(
                   child: Text(
-                    "Local video",
+                    "Waiting for remote user...",
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
               ),
             ),
-            controller.localRenderer.obs,
+            controller.remoteRenderer.obs,
           ),
-          // Room ID (Sağ üstte, radiuslu)
+          // Local user video
+          Obx(
+                () => isLocalVideoLarge.value
+                ? Positioned.fill(
+              child: GestureDetector(
+                onTap: () {
+                  isLocalVideoLarge.value = !isLocalVideoLarge.value;
+                },
+                child: ObxValue<Rx<RTCVideoRenderer>>(
+                      (data) => data.value.srcObject != null
+                      ? RTCVideoView(
+                    data.value,
+                    mirror: true,
+                    objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
+                  )
+                      : Container(
+                    color: Colors.black,
+                    child: const Center(
+                      child: Text(
+                        "Local video",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  controller.localRenderer.obs,
+                ),
+              ),
+            )
+                : Positioned(
+              left: localVideoPosition.value.dx,
+              top: localVideoPosition.value.dy,
+              child: Draggable(
+                feedback: ObxValue<Rx<RTCVideoRenderer>>(
+                      (data) => Container(
+                    width: 150,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: data.value.srcObject != null
+                        ? RTCVideoView(
+                      data.value,
+                      mirror: true,
+                      objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
+                    )
+                        : Container(
+                      color: Colors.black,
+                      child: const Center(
+                        child: Text(
+                          "Local video",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  controller.localRenderer.obs,
+                ),
+                childWhenDragging: Container(),
+                onDragEnd: (details) {
+                  localVideoPosition.value = details.offset;
+                },
+                child: GestureDetector(
+                  onTap: () {
+                    isLocalVideoLarge.value = !isLocalVideoLarge.value;
+                  },
+                  child: ObxValue<Rx<RTCVideoRenderer>>(
+                        (data) => Container(
+                      width: 150,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white, width: 2),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: data.value.srcObject != null
+                          ? RTCVideoView(
+                        data.value,
+                        mirror: true,
+                        objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
+                      )
+                          : Container(
+                        color: Colors.black,
+                        child: const Center(
+                          child: Text(
+                            "Local video",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    controller.localRenderer.obs,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // Room ID
           Positioned(
             top: 20,
             right: 20,
